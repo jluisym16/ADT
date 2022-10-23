0e2735a52251c2c4b29d3142e7d817ce0d27df34
jluisym16 <jluisym16@educastur.es>
2022-10-23 19:16:03 +0200

    askd

--- src/ejercicio_01/Ejercicio1_4_Borrado.java
+++ src/ejercicio_01/Ejercicio1_4_Borrado.java
@@ -1,5 +1,31 @@
 package ejercicio_01;
 
-public class Ejercicio1_4_Borrado {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
 
+public class Ejercicio1_4_Borrado {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	File f = new File ("J:\\ADT\\cosa.dat");
+	RandomAccessFile r = new RandomAccessFile(f, "rw");
+	System.out.println("Introduzca el ID del empleado a modificar: ");
+	int id = Integer.parseInt(br.readLine());
+	int idDel = -1;
+	int depDel = 0;
+	double salDel = 0;
+	long pos = (id - 1) * 36;
+	char [] apellido = new char[10];
+	char aux;
+	for (int i = 0; i < 1; i++) {
+		aux = (char) id;
+		apellido[i] = aux;
+	}
+	r.seek(pos);
+	r.writeInt(idDel);
+	String a = new String (apellido);
+	r.writeChars(a);
+	r.writeInt(depDel);
+	r.writeDouble(salDel);
+}
 }
--- src/ejercicio_01/Ejercicio1_4_Mod.java
+++ src/ejercicio_01/Ejercicio1_4_Mod.java
@@ -1,5 +1,36 @@
 package ejercicio_01;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
+
 public class Ejercicio1_4_Mod {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		File fic = new File ("J:\\ADT\\cosa.dat");
+		RandomAccessFile file = new RandomAccessFile(fic, "rw");
+		System.out.println("Introduzca el ID del empleado a modificar: ");
+		int id = Integer.parseInt(br.readLine());
+		long pos = (id - 1) * 36;
+		file.seek(pos + 4);
+		char [] apellido = new char[10];
+		char aux;
+		for (int i = 0; i < apellido.length; i++) {
+			aux = file.readChar();
+			apellido[i] = aux;
+		}
+		String a = new String (apellido);
+		file.seek(pos + 28);
+		double sal = file.readDouble();
+		double sal_old = sal;
+		System.out.println("Introduzca el salario a añadir: ");
+		double add = Double.parseDouble(br.readLine());
+		sal += add;
+		file.seek(pos + 28);
+		file.writeDouble(sal);
+		System.out.println("apellido: " + a.trim() + "salario antes: " + sal_old + "salario ahora: " + sal );
+	}
 
 }
